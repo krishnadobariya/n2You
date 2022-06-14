05925e6ba1c5131ab979bbd538124a65c5653061
krishnadobariya <krishnadobariya3488@gmail.com>
2022-06-14 15:59:30 +0530

    UPDATE: make image live

---
+++ src/utils/cloudinary.utils.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2
+
+
+cloudinary.config({
+    cloud_name: 'justhire',
+    api_key: '625429692641811',
+    api_secret: 'rRqC1PYNpYNxVccJQqcjr5e8oc0'
+})
+
+module.exports = cloudinary;
