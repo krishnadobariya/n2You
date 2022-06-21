47bdc7d16c9032b5c73395712434291dce8b6473
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-06-21 10:49:10 +0530

    Update APIResponse.js

--- src/helper/APIResponse.js
+++ src/helper/APIResponse.js
@@ -1,17 +1,21 @@
 class APIResponse {
-    constructor(message = '', status = false, code = 200, data = null, error = null) {
+    constructor(message = '', status = false, code = 200, statusCode = 1, data = null, error = null) {
         if (message) {
             this.message = message
-        }
-        if (code) {
-            this.code = code
-        }
-        if (data) {
-            this.data = data
         }
         if (status) {
             this.status = status
         }
+        if (code) {
+            this.code = code
+        }
+        if (statusCode) {
+            this.statusCode = statusCode
+        }
+        if (data) {
+            this.data = data
+        }
+
         if (error) {
             this.error = error
         }
