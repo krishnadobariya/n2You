a04b7c6352fdd32d136c287fb590f32c9751a3b3
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-06-21 09:32:26 +0530

    Update request.routes.js

--- src/routes/request.routes.js
+++ src/routes/request.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const requestController = require("../controller/request.controller");
 
 router.post("/send/:userEmail/:RequestedEmail", requestController.sendRequest);
-router.post("/userAcceptedRequesteOrNot/:email", requestController.userAcceptedRequesteOrNot);
+router.post("/userAcceptedRequesteOrNot/:userId/:email", requestController.userAcceptedRequesteOrNot);
 router.post("/showPostsOnalyAcceptedPerson/:userEmail/:RequestedEmail", requestController.showPostsOnalyAcceptedPerson);
 
 module.exports = router
