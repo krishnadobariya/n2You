340793af90172b19647843ba5da72dd87bb92315
krishnadobariya <krishnadobariya3488@gmail.com>
2022-06-21 15:20:16 +0530

    ADD: userwise all detail get

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,4 +7,7 @@ const userController = require("../controller/user.controller");
 
 router.post('/register', upload.array('photo'), userController.userRegister);
 router.get('/search/:userEmail', userController.searchFriend);
+router.get('/view/:UserId', userController.getDataUserWise);
+
+
 module.exports = router;
