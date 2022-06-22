c19b4297de598ff745031367dee57c26004e8653
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-06-22 09:04:04 +0530

    Update comment.routes.js

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -8,5 +8,6 @@ router.post('/reply/:postId/:userId/:commentId', commentController.replyComment)
 router.put('/edit/:PostId/:UserId/:commentId', commentController.editComment);
 router.delete('/delete/:PostId/:UserId/:commentId' , commentController.deleteComment);
 router.put('/replyEdit/:PostId/:UserId/:commentId/:commentReplayId' , commentController.replyCommentEdit);
+router.delete('/replyDelete/:post_id/:user_id/:comment_id/:comment_reply_id' , commentController.replyCommitDelete);
 
 module.exports = router;
