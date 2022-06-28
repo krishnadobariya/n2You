b407118c88f13358e8b2f19013f2797749ddb705
krishnadobariya <krishnadobariya3488@gmail.com>
2022-06-28 10:32:36 +0530

    UPDATE : profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,7 @@ const upload = require('../utils/multer.userImages.utils');
 const userController = require("../controller/user.controller");
 
 router.post('/register', upload.array('photo'), userController.userRegister);
+router.post('/update/:user_id', upload.array('photo'), userController.userUpdate);
 router.get('/search/:user_email', userController.searchFriend);
 router.get('/view/:user_id', userController.getDataUserWise);
 router.get('/nearestMe/:user_id', userController.userNearestMe);
