b4dbd36b74b0498e1220c3ecb7d2d722dc69319a
krishnadobariya <krishnadobariya3488@gmail.com>
2022-07-01 18:08:41 +0530

    UPDATE: get Polyamorous User

--- src/controller/polyamorous/dating.controller.js
+++ src/controller/polyamorous/dating.controller.js
@@ -48,12 +48,20 @@ exports.getUserWhichNotChoiceForLikeOrDislike = async (req, res, next) => {
                         if (checkUserInLike || checkUserInDisLike) {
 
                         } else {
+
+                            let brithDate = new Date(allUser.birthDate);
+                            brithDate = brithDate.getFullYear();
+                            let currentDate = new Date(Date.now());
+                            currentDate = currentDate.getFullYear();
+
+                            const age = currentDate - brithDate
+
                             const userDetail = {
                                 name: allUser.firstName,
-                                Bio: allUser.Bio,
-                                hopingToFind: allUser.hopingToFind,
-                                jobTitle: allUser.jobTitle,
-                                wantChildren: allUser.wantChildren
+                                gender: allUser.identity,
+                                age: age,
+                                photo: allUser.photo[0]
+
                             }
                             response.push(userDetail)
                         }
@@ -151,9 +159,6 @@ exports.matchTable = async (req, res, next) => {
                         }
                     }
                 }
-
-
-
             }
 
             const allMatchUser = await matchUserModel.findOne({
@@ -177,3 +182,67 @@ exports.matchTable = async (req, res, next) => {
 }
 
 
+exports.getPolyamorousUser = async (req, res, next) => {
+    try {
+
+        const findUser = await userModel.findOne({
+            _id: req.params.user_id
+        })
+
+        if (findUser == null) {
+            res.status(status.NOT_FOUND).json(
+                new APIResponse("User Not Found", "false", 404, "0")
+            );
+        } else {
+
+            const findPolyamorousUser = await userModel.findOne({
+                _id: req.params.user_id,
+                polyDating: "polyamorous"
+            })
+
+            if (findPolyamorousUser == null) {
+                res.status(status.NOT_FOUND).json(
+                    new APIResponse("no User Found Which is a Polyamorous ", "false", 404, "0")
+                );
+            } else {
+
+                let brithDate = new Date(findPolyamorousUser.birthDate);
+                brithDate = brithDate.getFullYear();
+                let currentDate = new Date(Date.now());
+                currentDate = currentDate.getFullYear();
+
+                const age = currentDate - brithDate
+
+                const response = {
+                    email: findPolyamorousUser.email,
+                    firstName: findPolyamorousUser.firstName,
+                    gender: findPolyamorousUser.identity,
+                    age: age,
+                    relationshipSatus: findPolyamorousUser.relationshipSatus,
+                    IntrestedIn: findPolyamorousUser.IntrestedIn,
+                    Bio: findPolyamorousUser.Bio,
+                    photo: findPolyamorousUser.photo,
+                    hopingToFind: findPolyamorousUser.hopingToFind,
+                    jobTitle: findPolyamorousUser.jobTitle,
+                    wantChildren: findPolyamorousUser.wantChildren,
+                    phoneNumber: findPolyamorousUser.phoneNumber,
+                    bodyType: findPolyamorousUser.extraAtrribute.bodyType,
+                    height: findPolyamorousUser.extraAtrribute.height,
+                    smoking: findPolyamorousUser.extraAtrribute.smoking,
+                    drinking: findPolyamorousUser.extraAtrribute.drinking,
+                    hobbies: findPolyamorousUser.extraAtrribute.hobbies
+                }
+                res.status(status.OK).json(
+                    new APIResponse("get Polyamorous User", "true", 200, "1", response)
+                );
+
+            }
+        }
+
+    } catch (error) {
+        console.log("Error:", error);
+        res.status(status.INTERNAL_SERVER_ERROR).json(
+            new APIResponse("Something Went Wrong", false, 500, "0", error.message)
+        );
+    }
+}
