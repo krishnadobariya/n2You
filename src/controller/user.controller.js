847bdfa3005ab87469fd3b72c0a7d343790d1436
krishnadobariya <krishnadobariya3488@gmail.com>
2022-08-16 09:38:19 +0530

    update: cloudinary

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -15,7 +15,8 @@ exports.userRegister = async (req, res, next) => {
         const cloudinaryImageUploadMethod = async file => {
             return new Promise(resolve => {
                 cloudinary.uploader.upload(file, (err, res) => {
-                    if (err) return res.status(500).send("upload image error")
+                    console.log(err);
+                    // if (err) return res.send("upload image error")
                     resolve({
                         res: res.secure_url
                     })
--- src/utils/cloudinary.utils.js
+++ src/utils/cloudinary.utils.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2
 
 
 cloudinary.config({
-    cloud_name: 'justhire',
-    api_key: '625429692641811',
-    api_secret: 'rRqC1PYNpYNxVccJQqcjr5e8oc0'
+    cloud_name: 'dts1dlmxw',
+    api_key: '414977289425792',
+    api_secret: 'CDkm4O9NuMlBe6KAEapRX5gDm5o'
 })
 
 module.exports = cloudinary;
