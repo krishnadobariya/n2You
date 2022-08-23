5699e4405172a971946c14351a57496d62474e4a
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-08-23 15:59:47 +0530

    Update user.controller.js

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -279,14 +279,46 @@ exports.userLogin = async (req, res, next) => {
 
         const findUser = await userModel.findOne({
             email: req.body.email
-        }).select("_id , email , password")
+        })
 
         if (findUser) {
 
             if (req.body.password == findUser.password) {
+
+
+                const data = {
+                    _id: findUser._id,
+                    polyDating: findUser.polyDating,
+                    HowDoYouPoly: findUser.HowDoYouPoly,
+                    loveToGive: findUser.loveToGive,
+                    polyRelationship: findUser.polyRelationship,
+                    email: findUser.email,
+                    firstName: findUser.firstName,
+                    birthDate: findUser.birthDate,
+                    identity: findUser.identity,
+                    relationshipSatus: findUser.relationshipSatus,
+                    IntrestedIn: findUser.intrestedIn,
+                    Bio: findUser.Bio,
+                    photo: findUser.photo,
+                    longitude: findUser.location.coordinates[0],
+                    latitude: findUser.location.coordinates[1],
+                    fcm_token: findUser.fcm_token,
+                    hopingToFind: findUser.hopingToFind,
+                    jobTitle: findUser.jobTitle,
+                    wantChildren: findUser.wantChildren,
+                    bodyType: findUser.extraAtrribute.bodyType,
+                    height: findUser.extraAtrribute.height,
+                    smoking: findUser.extraAtrribute.smoking,
+                    drinking: findUser.extraAtrribute.drinking,
+                    hobbies: findUser.extraAtrribute.hobbies,
+                    phoneNumber: findUser.phoneNumber,
+                    countryCode: findUser.countryCode,
+                    password: findUser.password
+                }
+               
                 res.status(status.OK).json(
 
-                    new APIResponse("login success", "true", 200, "1")
+                    new APIResponse("login success", "true", 200, "1" , data)
                 )
             } else {
                 res.status(status.NOT_FOUND).json(
