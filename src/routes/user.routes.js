6c583dd512038e73194fc5a69fa24b18cad61159
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-08-25 15:37:00 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,7 @@ router.post('/login', userController.userLogin);
 router.put('/update/:user_id', upload.array('photo'), userController.userUpdate);
 router.put('/update/token/:user_id', userController.tokenUpdate);
 router.get('/search/:user_id', userController.searchFriend);
-router.get('/view/:user_id', userController.getDataUserWise);
+router.get('/view/:user_id/:req_user_id', userController.getDataUserWise);
 router.get('/storeBasketValue/:user_id', userController.storeBasketValue);
 router.get('/yesBasket/:user_id/:request_user_id', userController.yesBasket);
 router.get('/noBasket/:user_id/:request_user_id', userController.noBasket);
