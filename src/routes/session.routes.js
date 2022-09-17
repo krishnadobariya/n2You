b3a8a2c70ceea3a87a37c106d97a5cb84c3e834d
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-09-17 17:29:08 +0530

    Update session.routes.js

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -8,4 +8,5 @@ router.get('/public/:user_id', sessionController.publicSession);
 router.get('/invited/:user_id', sessionController.invitedInSession);
 router.get('/my/:user_id', sessionController.mySession);
 router.get('/end/:session_id', sessionController.endSession);
+router.get('/raisHandList/:session_id' , sessionController.raisHandList);
 module.exports = router;
