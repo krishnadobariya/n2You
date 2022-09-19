41659be9d8a6c96a510f9308a8df8b8f0323bcdc
Krishna <88829507+krishnadobariya@users.noreply.github.com>
2022-09-19 15:16:19 +0530

    Update session.routes.js

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../utils/multer.postImages.utils");
+
 
 const sessionController = require("../controller/session.controller");
 
@@ -10,4 +12,7 @@ router.get('/my/:user_id', sessionController.mySession);
 router.get('/end/:session_id', sessionController.endSession);
 router.get('/raisHandList/:session_id' , sessionController.raisHandList);
 router.get('/detail/:session_id' , sessionController.sessionDetail);
+router.get('/uploadImages/:session_id/:user_id' , upload.array('upload') ,sessionController.uploadImages);
+router.get('/uploadVideos/:session_id/:user_id' , upload.array('upload') ,sessionController.uploadVideos);
+router.get('/getUploadeVedioOrImages/:session_id' , sessionController.getUploadeVedioOrImages);
 module.exports = router;
