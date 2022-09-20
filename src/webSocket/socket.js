4c2a45be98133f3333fbe7ca3f217d75fe6173da
krishnadobariya <krishnadobariya3488@gmail.com>
2022-09-20 16:17:42 +0530

    add : thumbup in session

--- src/webSocket/socket.js
+++ src/webSocket/socket.js
@@ -2846,6 +2846,8 @@ function socket(io) {
                     for (const sendComment of AllJoinUser) {
 
                         if ((sendComment.userId).toString() == (arg.user_id).toString()) {
+                            
+                        } else {
 
                             const findUser = await userModel.findOne({
                                 _id: arg.user_id
@@ -2860,22 +2862,6 @@ function socket(io) {
 
                             const userRoom = `User${sendComment.userId}`
                             io.to(userRoom).emit("commentResponse", commentData);
-
-                        } else {
-
-                            // const findUser = await userModel.findOne({
-                            //     _id: arg.user_id
-                            // })
-                            // const commentData = {
-                            //     userId: sendComment.userId,
-                            //     comment: arg.comment,
-                            //     userName: findUser.firstName,
-                            //     profile: findUser.photo[0] ? findUser.photo[0].res : "",
-                            //     status: sendComment.status
-                            // }
-
-                            // const userRoom = `User${sendComment.userId}`
-                            // io.to(userRoom).emit("commentResponse", commentData);
 
                         }
                     }
